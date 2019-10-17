51af2079aa5abea32623e0292ad6195dd738497f
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-10-17 16:38:39 -0400

    adds collection view cell objects and creates outlets

---
+++ weather/Views/WeatherCell.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherCell.swift
+//  weather
+//
+//  Created by Radharani Ribas-Valongo on 10/17/19.
+//  Copyright © 2019 Radharani Ribas-Valongo. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UICollectionViewCell {
+    
+    //MARK: -- Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var hiTemp: UILabel!
+    @IBOutlet weak var loTemp: UILabel!
+    
+    //MARK: -- Functions
+}
