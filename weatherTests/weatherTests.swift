4fad31e69ec434ae35e3ad24f2bae2da0e650706
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-10-17 15:59:29 -0400

    intial commit

---
+++ weatherTests/weatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherTests.swift
+//  weatherTests
+//
+//  Created by Radharani Ribas-Valongo on 10/17/19.
+//  Copyright © 2019 Radharani Ribas-Valongo. All rights reserved.
+//
+
+import XCTest
+@testable import weather
+
+class weatherTests: XCTestCase {
+    
+   
+    
+
+}
